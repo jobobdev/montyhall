46d77d4acb615f2324ee4f5e00b64c6e6b42a973
raoner <suitable1125@gmail.com>
2022-05-09 22:56:32 +0900

    Implement : create class montyHall __init__ c,s,o_door set
    participant가 선택한 door(s_door) 번호 를
    생성하는 class를 설계하였다.
    
    출력 하는 프로그램을 하단부에 작성하였다.
    - 결과값 확인 후 주석처리 필요함

--- montyhall.py
+++ montyhall.py
@@ -1,4 +1,22 @@
 # 이제 몬티홀을 만들어 봅시다.
 # 자료형: int
 # 변수 명: 선택한 문 = 's_door', 자동차 문 = 'c_door', 사회자가 열어줄 문 = 'o_door'
+import random as rd
+
+class montyHall():
+  def __init__(self):
+    self.c_door = rd.randrange(1,4)
+    self.s_door = rd.randrange(1,4)
+    self.o_door = 0
+
+# 1st 개발(본인)은 
+# 클래스 self.s_door에 참여자가 선택한 문 번호를 저장함(1~3 중의 1개의 임의 수)
+# 클래스 self.c_door에 자동차가 들어 있는 문 번호를 저장함(1~3 중의 1개의 임의 수)
+# 2nd 개발부분에서 사용할 < 객체명 = montyHall() >(으)로 인스턴스를 할당하여 
+# 결정 된 수들(s_door, c_door, o_door)을 접근할 수 있습니다.
+
+# 사용예는 아래와 같다.
+
+FirstStepResult = montyHall()
+print(FirstStepResult.c_door, FirstStepResult.s_door)
 
